1cea9f7201506cd116863a9d6140fda35c25070f
Faustas <faustas.slekonis@gmail.com>
2025-05-22 12:07:21 +0300

    dome

---
+++ darzas/js/spalvos.js
@@ -0,0 +1,15 @@
+import { header } from "./header.js";
+
+header();
+
+document.querySelector('title').textContent = 'Spalvos';
+
+const colors = ['yellow', 'red', 'green', 'blue', 'orange', 'aqua'];
+const ulDOM = document.querySelector('.colors');
+
+ulDOM.innerHTML = '';
+
+for (const color of colors) {
+    ulDOM.innerHTML += `<li style="background-color: ${color};">Spalva: ${color}</li>`;
+}
+
